8e22b2d396dd72eb003bfb1a532cd1eb22f6a213
lbuell <liamcbuell@gmail.com>
2014-02-23 15:07:19 -0800

    ch4 exercises

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -9,34 +9,13 @@ module Rulers
     def call(env)
       if env['PATH_INFO'] == '/favicon.ico'
         return [404,
-                {'Content-Type' => 'text/html'}, []]
+          {'Content-Type' => 'text/html'}, []]
       end
-      if env['PATH_INFO'] == '/'
-        return [301, {'Content-Type' => 'text/html', 'Location' => '/quotes/a_quote'}, []]
-      end
-      begin
-        klass, act = get_controller_and_action(env)
-        controller = klass.new(env)
-        text = controller.send(act)
-        [200, {'Content-Type' => 'text/html'}, [text]]
-      rescue Exception
-        [500, {'Content-Type' => 'text/html'}, ['ERROR.']]
-      end
-    end
-  end
 
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
-    end
-    def controller_name
-      klass = self.class
-      klass = klass.to_s.gsub /Controller$/, ""
-      Rulers.to_underscore klass
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {'Content-Type' => 'text/html'}, [text]]
     end
   end
 end
